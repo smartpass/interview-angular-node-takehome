8a25929f236b8e82930106d24403b0db7cf4684b
Tamer Wahba <tamer@smartpass.app>
2024-02-13 10:23:13 -0500

    add material design with basic navbar and routing

--- packages/client/src/app/app.component.ts
+++ packages/client/src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { WebsocketReceiverComponent } from './websocket-receiver/websocket-receiver.component';
-import { StudentListComponent } from './student-list/student-list.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StudentListComponent, WebsocketReceiverComponent],
+  imports: [
+    NavigationComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
 }
