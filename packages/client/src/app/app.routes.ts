ddfa3802a6e1df3f2719aa62fbd0e5f602a735e9
Tamer Wahba <tamer@smartpass.app>
2024-02-20 17:10:13 -0500

    remove default angular home route with associated components

--- packages/client/src/app/app.routes.ts
+++ packages/client/src/app/app.routes.ts
@@ -1,16 +1,14 @@
 import { Routes } from '@angular/router'
 
 import { ActivePassesComponent } from './active-passes/active-passes.component'
-import { HomeComponent } from './home/home.component'
 import { LocationsTableComponent } from './locations-table/locations-table.component'
 import { PassesTableComponent } from './passes-table/passes-table.component'
 import { StudentsTableComponent } from './students-table/students-table.component'
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'students', component: StudentsTableComponent },
   { path: 'locations', component: LocationsTableComponent },
   { path: 'passes', component: PassesTableComponent },
   { path: 'active-passes', component: ActivePassesComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' },
+  { path: '**', pathMatch: 'full', redirectTo: 'active-passes' },
 ]
